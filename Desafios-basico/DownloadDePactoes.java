c9881282f2740a9e019f49410f59f01c8e44b2e8
Darlene Moura <mdarlenemoura@gmail.com>
2023-01-06 20:42:49 -0300

    commit inicial

---
+++ Desafios-basico/DownloadDePactoes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+    
+public class Program {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int entrada = leitor.nextInt();
+        int mesada = 50;
+    
+        int calculo= (entrada * mesada);
+        
+
+         System.out.println(calculo);
+      
+    }
+}
